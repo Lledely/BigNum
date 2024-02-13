95db3d1c9185a1698e948c0bd6c9d1d325fbbea1
Lledely <nemtsov.vovan@yandex.ru>
2024-02-14 00:28:27 +0300

    added something in header file

--- headers/BigNum.h
+++ headers/BigNum.h
@@ -1,1 +1,26 @@
 #pragma once
+
+#include <vector>
+#include <string>
+#include <cmath>
+#include <inttypes.h>
+
+#define CHUNK 64
+#define CHUNK_LEN 9
+#define CHUNK_TYPE uint64_t
+
+namespace bignum{
+
+    class BigNum {
+
+        public:
+
+            BigNum(const std::string &num);
+
+        private:
+
+            std::vector<CHUNK_TYPE> _chunks;
+
+    };
+
+}
