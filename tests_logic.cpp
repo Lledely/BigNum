d8b9047a6231b81a641a70662c2e84834c94e358
Lledely <nemtsov.vovan@yandex.ru>
2024-02-21 23:50:13 +0300

    added logic tests

--- tests_logic.cpp
+++ tests_logic.cpp
@@ -1,6 +1,23 @@
 #include <BigNum.h>
 
 int main() {
+    bignum::BigNum a("420.69");
+    bignum::BigNum b(0);
+    bignum::BigNum c = "-0."_bn;
+    bignum::BigNum d(-1231231233ll);
+
+    std::vector<bignum::BigNum> nums = {a, b, c, d};
+
+    for (bignum::BigNum i: nums) {
+        for (bignum::BigNum j: nums) {
+            std::cout << i.to_string() << " <= " << j << " - " << ((i <= j) ? "true" : "false") << std::endl;
+            std::cout << i.to_string() << " < " << j << " - " << ((i < j) ? "true" : "false") << std::endl;
+            std::cout << i.to_string() << " >= " << j << " - " << ((i >= j) ? "true" : "false") << std::endl;
+            std::cout << i.to_string() << " > " << j << " - " << ((i > j) ? "true" : "false") << std::endl;
+            std::cout << i.to_string() << " == " << j << " - " << ((i == j) ? "true" : "false") << std::endl;
+            std::cout << i.to_string() << " != " << j << " - " << ((i != j) ? "true" : "false") << std::endl;
+        }
+    }
 
     return 0;
 }
